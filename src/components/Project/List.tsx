66be54a1f7c717aaaef01434f5781ed87a3ad5ab
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2021-02-18 00:21:45 +0900

    +fu add useCallback

--- src/components/Project/List.tsx
+++ src/components/Project/List.tsx
@@ -4,10 +4,14 @@ import { Project } from '../../api/project'
 const ProjectList: FC<{ projects: Project[] }> = ({ projects }) => {
   // REACT_HOOKS_BEST_PRACTICE: Using useMemo for more fine-grained optimized rendering. 
   // Different from memo, React.memo == PureComponent
-  return useMemo(() =>
-    <div className="App">
-      {projects.map(({ id, name }) => <div key={id}>{name}</div>)}
-    </div>, [projects])
+  return useMemo(() => (
+    <>
+      {
+        projects.map(({ id, name }) =>
+          <div key={id}>{name}</div>)
+      }
+    </>
+  ), [projects])
   // Use useMemo to wrap the rendering code.
   // So that even if the function Child is re-executed due to changes in props, 
   // as long as the projects used by the rendering function has not changed, it will not be re-rendered
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,21 +1,26 @@
-import React, { useEffect,useCallback, FC } from 'react'
+import React, { useEffect, useMemo, useCallback, FC } from 'react'
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState, Dispatch } from '../store'
 import ProjectList from '../components/Project/List'
+import ProjectEdit from '../components/Project/Edit'
+import ProjectCreate from '../components/Project/Create'
 
 // REACT_HOOKS_BEST_PRACTICE: Function component is defined by const + arrow function
 const App: FC = () => {
   const projects = useSelector((state: RootState) => state.project.listData)
   const dispatch = useDispatch<Dispatch>()
+  const fetchData = useCallback(() => dispatch.project.listAsync(), [])
 
   useEffect(() => {
-    dispatch.project.listAsync()
-  }, [])
+    fetchData()
+  }, [fetchData])
 
-  return (
+  return useMemo(() => <div className="App">
+    <ProjectCreate />
+    <ProjectEdit />
     <ProjectList projects={projects} />
-  )
+  </div>, [])
 }
 
 export default App
